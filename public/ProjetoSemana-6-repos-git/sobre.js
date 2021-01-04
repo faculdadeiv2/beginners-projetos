d6f208940276372a66508d9e427fd49a07815773
Lucas Favareto Santos <lucasfavaretosantos98@gmail.com>
2021-01-04 14:51:50 -0300

    Update sobre.js

--- public/ProjetoSemana-6-repos-git/sobre.js
+++ public/ProjetoSemana-6-repos-git/sobre.js
@@ -34,21 +34,6 @@ xhr.onreadystatechange = function () {
             <p class="mdl-card__supporting-text">Linguagem Predominante: `+dadosJSONObj.language+`</p>
             <iframe class="mdl-card__supporting-text scroll" style="height:220px"src="images/`+dadosJSONObj.language+`.txt"></iframe>
             <img src="images/`+dadosJSONObj.language +`.png"></img>`
-            
-        
-        
-        
-        
-           /*  "<div id=\"divisao\" align=\"center\"><p>" + "Username: " + dadosJSONObj.owner.login + "</p><br>" +
-            "<p>" + "Id do Repositório: " + dadosJSONObj.id + "</p><br>" +
-            "<p><img id=\"imagem\" align=\"center\" src=\"images/otavio.jpg\"></img></p><br>" +
-            "<p id=\"seguidores\"><a href=\"('" + dadosJSONObj.following_url + "')\">Link Para Folllowers" + "</a></p>" +
-            "<p>" + "Descrição do Repositório: " + dadosJSONObj.description + "</p><br></div>" +
-            "<div id=\"link\" align=\"center\"><p><a href=\"('" + dadosJSONObj.collaborators_url + "')\">Link Para Colaboradores" + "</a></p>" +
-            "<p><a href=\"('" + dadosJSONObj.branches_url + "')\">Link Para Branches" + "</a></p>" +
-            "<p><a href=\"('" + dadosJSONObj.commits_url + "')\">Link Para Commits" + "</a></p></div>" +
-            "<div id=\"linguagem\" align=\"center\"><p id=\"titulo\">" + "Linguagem Predominante: " + dadosJSONObj.language + "<p><br>" +
-            "<iframe  class=\"texta\" scrolling=\"no\" src='images/" + dadosJSONObj.language + ".txt'></iframe>" + "<img id=\"lingimg\"width=\"30%\" src='images/" + dadosJSONObj.language + ".png'></img></div><br>" */
     }
 }
 xhr.send();
